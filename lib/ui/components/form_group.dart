8a799990d12267132d9ac8b59454dbb3875e4dbe
Hidayatullah <ayat.kyo@gmail.com>
2022-09-28 10:31:35 +0800

    chore: add formgroup component
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

---
+++ lib/ui/components/form_group.dart
@@ -0,0 +1,86 @@
+import 'package:mim_generator/styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FormGroup extends StatelessWidget {
+  FormGroup({
+    super.key,
+    required this.label,
+    required this.placeholder,
+    this.ctrl,
+    this.optional = false,
+    this.multiline = false,
+    this.maxLength,
+    this.keyboardType,
+    this.onChanged,
+  });
+
+  String label;
+  String placeholder;
+  TextEditingController? ctrl;
+
+  bool optional;
+  bool multiline;
+
+  int? maxLength;
+  TextInputType? keyboardType;
+  Function(String)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                label,
+                style: AppTextStyles.formLabel,
+              ),
+              if (optional)
+                Text(
+                  "Opsional",
+                  style: AppFonts.secondary(
+                    fontSize: 11,
+                    color: Colors.black.withOpacity(0.65),
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+            ],
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            controller: ctrl,
+            keyboardType: keyboardType,
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              isDense: true,
+              filled: true,
+              fillColor: Colors.black.withOpacity(0.1),
+              border: OutlineInputBorder(
+                borderRadius: AppBorderRadius.normal,
+                borderSide: BorderSide.none,
+                gapPadding: 0,
+              ),
+              hintText: placeholder,
+              hintStyle: AppFonts.secondary(
+                color: Colors.black.withOpacity(0.35),
+              ),
+            ),
+            maxLines: multiline ? null : 1,
+            maxLength: maxLength,
+            style: AppFonts.secondary(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
